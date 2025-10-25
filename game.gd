b38f827e2d1a0cbc00d1d97125e34db4b1917d86
Matthew LettucePie <thelettucepie@gmail.com>
2025-10-25 11:20:02 -0400

    relocated input segment logic

--- game.gd
+++ game.gd
@@ -1,14 +1,62 @@
 extends Node
 class_name Game
 
-@onready var center_node : Node2D = $center
+@export var ring_center : Node2D
+var ring_levels : Array[Ring_Level] = []
+const DEF_click_max = 250.0
+const DEF_click_min = 50.0
+var click_max : float = DEF_click_max
+var click_min : float = DEF_click_min
+var segment_levels : PackedVector2Array = []
+
 
 func _ready() -> void:
 	if !get_window().size_changed.is_connected(recenter):
 		get_window().size_changed.connect(recenter)
+	if ring_center == null:
+		ring_center = find_child("center")
+	if ring_center != null:
+		for c in ring_center.get_children():
+			if c is Ring_Level:
+				ring_levels.append(c)
+	_calc_segment_levels(ring_levels.size())
 
 
 func recenter() -> void:
 	print("Recenter")
-	center_node.position = get_window().size / 2
-	print(center_node.position)
+	ring_center.position = get_window().size / 2
+	print(ring_center.position)
+	if ring_levels.size() > 0:
+		for ring_level in ring_levels:
+			ring_level.rescale()
+
+
+func _calc_segment_levels(steps : int):
+	segment_levels.clear()
+	segment_levels.resize(steps)
+	var new_max = click_max - click_min
+	var segment_size = new_max / steps
+	var x = 0
+	var y = segment_size
+	for i in steps:
+		segment_levels[i] = Vector2(x, y)
+		x = y
+		y += segment_size
+
+
+func _check_segment(dist : float) -> int:
+	var result : int = -1
+	for i in segment_levels.size():
+		var segment : Vector2 = segment_levels[i]
+		if dist >= segment.x and dist < segment.y:
+			result = i
+	return result
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			var local_pos : Vector2 = event.position - ring_center.global_position
+			var dist : float = Vector2.ZERO.distance_to(local_pos)
+			print(local_pos, " | ", dist)
+			print(_check_segment(dist - click_min))
--- ring_level.gd
+++ ring_level.gd
@@ -3,52 +3,12 @@ class_name Ring_Level
 
 @export var ring_level : int = 0
 @export var ring_scenes : Array[PackedScene] = []
-const DEF_click_max = 250.0
-const DEF_click_min = 50.0
-var click_max : float = DEF_click_max
-var click_min : float = DEF_click_min
-var segment_levels : PackedVector2Array = []
 
 
 func _ready():
 	var ring : Ring = ring_scenes.front().instantiate()
 	self.add_child(ring)
-	_calc_segment_levels()
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.pressed:
-			var local_pos : Vector2 = event.position - self.global_position
-			var dist : float = Vector2.ZERO.distance_to(local_pos)
-			print(local_pos, " | ", dist)
-			print(_check_segment(dist - click_min))
 
 
 func rescale():
 	print("Size Changing Stuff Here")
-	click_max = DEF_click_max * scale.x
-	click_min = DEF_click_min * scale.x
-	_calc_segment_levels()
-
-
-func _calc_segment_levels():
-	segment_levels.clear()
-	segment_levels.resize(4)
-	var new_max = click_max - click_min
-	var segment_size = new_max / 4
-	segment_levels[0] = Vector2(0, segment_size)
-	segment_levels[1] = Vector2(segment_levels[0].y, segment_levels[0].y + segment_size)
-	segment_levels[2] = Vector2(segment_levels[1].y, segment_levels[1].y + segment_size)
-	segment_levels[3] = Vector2(segment_levels[2].y, segment_levels[2].y + segment_size)
-
-
-func _check_segment(dist : float) -> int:
-	var result : int = -1
-	
-	for i in segment_levels.size():
-		var segment : Vector2 = segment_levels[i]
-		if dist >= segment.x and dist < segment.y:
-			result = i
-	
-	return result
