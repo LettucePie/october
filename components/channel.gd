5a13ce814f8be4240f947122f5e0919860747f0d
Matthew LettucePie <thelettucepie@gmail.com>
2025-11-03 21:38:49 -0500

    fixed channel boundaries

--- components/channel.gd
+++ components/channel.gd
@@ -3,6 +3,10 @@ extends Interactive
 class_name Channel
 
 const DEF_radius : float = 50.0
+const DEF_outer_arc_w : float = 18
+const DEF_outer_cap_r : float = 13
+const DEF_inner_arc_w : float = 16
+const DEF_inner_cap_r : float = 12
 @export_range(1.0, 4.0, 1.0) var radius_multiplier : float = 1.0
 @export_range(0.0, 360.0, 15.0) var point_a_angle : float = 90
 @export_range(0.0, 360.0, 15.0) var point_b_angle: float = 180
@@ -23,6 +27,7 @@ func _process(delta) -> void:
 
 
 func _draw() -> void:
+	## Backdrop Channel
 	## Main Arc
 	draw_arc(
 		self.position, 
@@ -30,23 +35,52 @@ func _draw() -> void:
 		deg_to_rad(point_a_angle),
 		deg_to_rad(point_b_angle),
 		arc_points,
-		Color.CHARTREUSE,
-		20,
+		Color.BLACK,
+		DEF_outer_arc_w,
 		true
 		)
 	## End Caps
 	draw_circle(
 		point_a,
-		15,
-		Color.CHARTREUSE,
+		DEF_outer_cap_r,
+		Color.BLACK,
 		true,
 		-1.0,
 		true
 	)
 	draw_circle(
 		point_b,
-		15,
-		Color.CHARTREUSE,
+		DEF_outer_cap_r,
+		Color.BLACK,
+		true,
+		-1.0,
+		true
+	)
+	## Inside Channel
+	## Main Arc
+	draw_arc(
+		self.position, 
+		DEF_radius * radius_multiplier + 25,
+		deg_to_rad(point_a_angle),
+		deg_to_rad(point_b_angle),
+		arc_points,
+		Color.WHITE,
+		DEF_inner_arc_w,
+		true
+		)
+	## End Caps
+	draw_circle(
+		point_a,
+		DEF_inner_cap_r,
+		Color.WHITE,
+		true,
+		-1.0,
+		true
+	)
+	draw_circle(
+		point_b,
+		DEF_inner_cap_r,
+		Color.WHITE,
 		true,
 		-1.0,
 		true
