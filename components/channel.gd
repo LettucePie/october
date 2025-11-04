67f35a65b4afe3c44dbb427cd98abb12c2880184
Matthew LettucePie <thelettucepie@gmail.com>
2025-11-04 13:45:01 -0500

    still struggling with 90 degree wraparound

--- components/channel.gd
+++ components/channel.gd
@@ -9,16 +9,16 @@ const DEF_inner_arc_w : float = 16
 const DEF_inner_cap_r : float = 12
 @export var lock_icon : Texture2D
 @export_range(1.0, 4.0, 1.0) var radius_multiplier : float = 1.0
-@export_range(0.0, 360.0, 15.0) var point_a_angle : float = 90
-@export_range(0.0, 360.0, 15.0) var point_b_angle: float = 180
-@export_range(0.0, 360.0, 15.0) var anchor_angle : float = 90
+@export_range(-180.0, 180.0, 15.0) var point_a_angle : float = 0.0
+@export_range(-180.0, 180.0, 15.0) var point_b_angle: float = 0.0
+@export_range(-180.0, 180.0, 15.0) var anchor_angle : float = 0.0
 var point_a : Vector2 = Vector2.ZERO
 var point_b : Vector2 = Vector2.ZERO
 var anchor_point : Vector2 = Vector2.ZERO
 var arc_points : int = 10
 var starting_rot : float = 0.0
-@onready var clamp_a : float = deg_to_rad(point_a_angle - (90 - anchor_angle))
-@onready var clamp_b : float = deg_to_rad(point_b_angle - (90 - anchor_angle))
+@onready var clamp_a : float = 0.0
+@onready var clamp_b : float = 0.0
 
 
 func _ready() -> void:
@@ -28,17 +28,17 @@ func _ready() -> void:
 
 func _process(delta) -> void:
 	point_a = self.position + (
-		Vector2.DOWN.rotated(deg_to_rad(point_a_angle)) * (DEF_radius * radius_multiplier + 25)
+		Vector2.RIGHT.rotated(deg_to_rad(point_a_angle + 180)) * (DEF_radius * radius_multiplier + 25)
 	)
 	point_b = self.position + (
-		Vector2.DOWN.rotated(deg_to_rad(point_b_angle)) * (DEF_radius * radius_multiplier + 25)
+		Vector2.RIGHT.rotated(deg_to_rad(point_b_angle + 180)) * (DEF_radius * radius_multiplier + 25)
 	)
 	arc_points = 10 + int(3 * radius_multiplier)
 	anchor_point = self.position + (
-		Vector2.DOWN.rotated(deg_to_rad(anchor_angle) - self.global_rotation) * (DEF_radius * radius_multiplier + 25)
+		Vector2.RIGHT.rotated(deg_to_rad(anchor_angle + 180) - self.global_rotation) * (DEF_radius * radius_multiplier + 25)
 	)
-	clamp_a = deg_to_rad(point_a_angle - (90 - anchor_angle))
-	clamp_b = deg_to_rad(point_b_angle - (90 - anchor_angle))
+	clamp_a = deg_to_rad(point_a_angle)
+	clamp_b = deg_to_rad(point_b_angle)
 	queue_redraw()
 
 
@@ -48,8 +48,8 @@ func _draw() -> void:
 	draw_arc(
 		self.position, 
 		DEF_radius * radius_multiplier + 25,
-		deg_to_rad(point_a_angle + 90),
-		deg_to_rad(point_b_angle + 90),
+		deg_to_rad(point_a_angle + 180),
+		deg_to_rad(point_b_angle + 180),
 		arc_points,
 		Color.BLACK,
 		DEF_outer_arc_w,
@@ -77,8 +77,8 @@ func _draw() -> void:
 	draw_arc(
 		self.position, 
 		DEF_radius * radius_multiplier + 25,
-		deg_to_rad(point_a_angle + 90),
-		deg_to_rad(point_b_angle + 90),
+		deg_to_rad(point_a_angle + 180),
+		deg_to_rad(point_b_angle + 180),
 		arc_points,
 		Color.WHITE,
 		DEF_inner_arc_w,
